4bdd4c4a2927d47e7a91c3a68d1601aed7472d64
Viorel Soltan <joomjoom2011@gmail.com>
2025-09-12 12:03:33 +0000

    swiper cng

--- app/components/HomeSlide.vue
+++ app/components/HomeSlide.vue
@@ -1,49 +1,58 @@
 <template>
-  <Swiper
-    :modules="['autoplay', 'effect-fade']"
-    :slides-per-view="1"
-    :loop="true"
-    :speed="2600"
-    :spaceBetween="20"
-    effect="fade"
-    :autoplay="{ delay: 2600, disableOnInteraction: true }"
+<Swiper
+  :modules="['autoplay', 'effect-fade']"
+  :slides-per-view="1"
+  :loop="true"
+  :speed="2600"
+  :spaceBetween="20"
+  effect="fade"
+  :autoplay="{ delay: 2600, disableOnInteraction: true }"
+>
+  <SwiperSlide
+    v-for="popular in popularMovies"
+    :key="popular.id"
+    class="animeslide-slide"
   >
-    <SwiperSlide
-      v-for="popular in popularMovies.value?.results || []"
-      :key="popular.id"
-      class="animeslide-slide"
+    <div
+      class="bg-slide"
+      :style="{
+        'background-image': `url(https://image.tmdb.org/t/p/original/${popular?.backdrop_path})`
+      }"
     >
-      <div
-        class="bg-slide"
-        v-bind:style="{
-          'background-image': `url(https://image.tmdb.org/t/p/original/${popular?.backdrop_path})`,
-        }"
-      >
-        <div class="container">
-          <div class="description-container">
-            <div class="description-card">
-              <div class="description-card__header" data-animate="bottom">
-                <h1>{{ popular.original_title }}</h1>
-              </div>
-              <div class="description-card__body" data-animate="bottom">
-                <p>{{ popular.overview }}</p>
-                <NuxtLink :to="`movies/${popular.id}`" class="button"
-                  >Read More
-                </NuxtLink>
-              </div>
+      <div class="container">
+        <div class="description-container">
+          <div class="description-card">
+            <div class="description-card__header" data-animate="bottom">
+              <h1>{{ popular.original_title }}</h1>
+            </div>
+            <div class="description-card__body" data-animate="bottom">
+              <p>{{ popular.overview }}</p>
+              <NuxtLink :to="`movies/${popular.id}`" class="button">Read More</NuxtLink>
             </div>
           </div>
         </div>
       </div>
-    </SwiperSlide>
-  </Swiper>
+    </div>
+  </SwiperSlide>
+</Swiper>
+
 </template>
+
 <script lang="ts" setup>
+import { ref } from 'vue'
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import 'swiper/css'
+import 'swiper/css/effect-fade'
+import 'swiper/css/autoplay'
 
-import { Popular } from '../types';
+//const popularMovies = ref<any[]>([])
 
-import { Swiper, SwiperSlide } from 'swiper/vue';
 
-const { data: popularMovies } = useFetch('popular', () => $fetch('/api/movies/popular'));
-//console.log(popularMovies.value);
+// fetch server-side / client
+const { data, pending, error } = await useFetch('/api/movies/popular')
+
+// când datele vin, le punem în ref
+if (data.value?.results) {
+  popularMovies.value = data.value.results
+}
 </script>
--- app/components/Upcoming.vue
+++ app/components/Upcoming.vue
@@ -5,55 +5,54 @@
         <h2>Upcoming Movies</h2>
       </div>
       <Swiper
-        :modules="[Navigation]"
-        :breakpoints="{
-          640: { slidesPerView: 1 },
-          768: { slidesPerView: 2 },
-          1024: { slidesPerView: 3 },
-          1280: { slidesPerView: 4 },
-        }"
-        :loop="true"
-        :spaceBetween="20"
-    :navigation="true"
-      >
-        <SwiperSlide v-for="up in upcomingMovies.value?.results || []" :key="up.id">
-          <div class="card">
-            <div class="card__header">
-              <img :src="`https://image.tmdb.org/t/p/w500/${up.poster_path}`" />
-            </div>
-            <div class="card__body">
-              <div class="card__body-title">
-                <h4>{{ up.original_title }}</h4>
-              </div>
-              <div class="flex card__details">
-                <div class="card__date">{{ up.release_date }}</div>
-                <div
-                  :class="[
-                    'card__average',
-                    up.vote_average >= 7 ? 'green' : 'yellow',
-                  ]"
-                >
-                  {{ up.vote_average }}
-                </div>
-              </div>
-
-              <NuxtLink :to="`movies/${up.id}`" class="button"
-                >Read More
-              </NuxtLink>
-            </div>
+  :modules="[Navigation]"
+  :breakpoints="{
+    640: { slidesPerView: 1 },
+    768: { slidesPerView: 2 },
+    1024: { slidesPerView: 3 },
+    1280: { slidesPerView: 4 }
+  }"
+  :loop="true"
+  :spaceBetween="20"
+  :navigation="true"
+>
+  <SwiperSlide v-for="up in upcomingMovies" :key="up.id">
+    <div class="card">
+      <div class="card__header">
+        <img :src="`https://image.tmdb.org/t/p/w500/${up.poster_path}`" />
+      </div>
+      <div class="card__body">
+        <h4>{{ up.original_title }}</h4>
+        <div class="flex card__details">
+          <div class="card__date">{{ up.release_date }}</div>
+          <div :class="['card__average', up.vote_average >= 7 ? 'green' : 'yellow']">
+            {{ up.vote_average }}
           </div>
-        </SwiperSlide>
-      </Swiper>
+        </div>
+        <NuxtLink :to="`movies/${up.id}`" class="button">Read More</NuxtLink>
+      </div>
+    </div>
+  </SwiperSlide>
+</Swiper>
     </div>
   </section>
 </template>
 <script lang="ts" setup>
+import { ref } from 'vue'
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import 'swiper/css'
+import 'swiper/css/navigation'
 
-import { Swiper, SwiperSlide } from 'swiper/vue';
-import Navigation from 'swiper/modules/navigation';
+import { Navigation } from 'swiper'
 
-const { data: upcomingMovies } = useFetch('upcoming', () => $fetch('/api/movies/upcoming'));
-//console.log(upcomingMovies.value);
+const upcomingMovies = ref<any[]>([])
 
-//console.log(upcomingMovies.value);
+// fetch-ul
+const { data } = await useFetch('/api/movies/upcoming')
+
+// când datele vin, le punem în ref-ul local
+if (data.value?.results) {
+  upcomingMovies.value = data.value.results
+}
 </script>
+
