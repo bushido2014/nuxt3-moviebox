9f1e291c373b8c1693195cd55a8cf60369297076
Viorel Soltan <joomjoom2011@gmail.com>
2025-09-16 17:06:02 +0300

    Update [...path].ts

--- server/api/movies/[...path].ts
+++ server/api/movies/[...path].ts
@@ -2,15 +2,19 @@ export default defineEventHandler(async (event) => {
   const config = useRuntimeConfig()
 
   let path = event.context.params?.path
-
-  // dacă path e string (ex: "search") îl transformăm în array
   if (!Array.isArray(path)) path = path ? [path] : []
 
-  const query = getQuery(event).query as string
+  const query = getQuery(event)
 
-  const url = query
-    ? `https://api.themoviedb.org/3/search/movie?api_key=${config.tmdbApiKey}&language=en-US&page=1&query=${encodeURIComponent(query)}`
-    : `https://api.themoviedb.org/3/movie/${path.join("/")}?api_key=${config.tmdbApiKey}&language=en-US`
+  let url = ''
+
+  if (path[0] === 'search') {
+    // ruta de search: /api/movies/search?query=...
+    url = `https://api.themoviedb.org/3/search/movie?api_key=${config.tmdbApiKey}&language=en-US&page=1&query=${encodeURIComponent(query.query || '')}`
+  } else {
+    // toate celelalte rute: /movie/{id} sau /movie/{id}/credits /recommendations
+    url = `https://api.themoviedb.org/3/movie/${path.join('/')}?api_key=${config.tmdbApiKey}&language=en-US`
+  }
 
   try {
     return await $fetch(url)
