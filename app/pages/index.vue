3a55f3c361f78478af03aecab94b48584a02cfcb
Viorel Soltan <joomjoom2011@gmail.com>
2025-09-17 16:17:51 +0300

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,14 +1,24 @@
 <template>
   <div class="home-page">
-    
+    <Loader v-if="isLoading" />
+    <div v-else>
     <HomeSlide />
     <TopRated />
     <Upcoming />
+      </div>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { ref, onMounted } from 'vue';  
+const isLoading = ref(true)
 
+onMounted(() => {
+  setTimeout(() => {
+    isLoading.value = false
+  }, 2000) 
+})
+  
   useHead({
   title: 'Movie Home Page',
   meta: [{ name: 'description', content: 'Movie all' }],
@@ -17,3 +27,4 @@
   },
 });
 </script>
+
