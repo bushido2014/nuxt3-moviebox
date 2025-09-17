889310b80b32a613b3b4564362f3d3df92195a8b
Viorel Soltan <joomjoom2011@gmail.com>
2025-09-17 13:49:36 +0000

    dlt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,26 +1,16 @@
 export default defineNuxtConfig({
   css: ['~/assets/css/main.css'],
   modules: ['nuxt-gtag'],
+   head: {
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt Movie App' },
+      { name: 'format-detection', content: 'telephone=no' },
+    ],
+  },
   gtag: {
     id: 'G-L1M6JHWR6T'
-  },
-  runtimeConfig: {
-    tmdbApiKey: process.env.TMDB_API_KEY, // server-only
-    tmdbBaseURL: 'https://api.themoviedb.org/3/',
-    public: {
-      apiBase: '/api' // <--- mereu local, Netlify se ocupă de funcții
-    }
-  },
-  nitro: {
-    preset: 'netlify',
-    experimental: {
-      wasm: false
-    },
-    routeRules: {
-      '/api/**': { 
-        cors: true,
-        headers: { 'Access-Control-Allow-Origin': '*' }
-      }
-    }
-  },
+  }
+ 
 })
